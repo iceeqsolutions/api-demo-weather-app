8df30292f3534c9f1af7cf67165c52358f40d463
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-07-24 17:18:02 +0300

    ADD env file and test settings
    - Add .env -file to project + add KEY to it
    - Add dotenv-webpack plugin
    - Add plugin to webpack.config file
    - Add new getWeatherData -module and link KEY to file
    - Run bild command and confirm output

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -30,5 +31,6 @@ module.exports = {
       template: "./src/index.html",
       scriptLoading: "defer",
     }),
+    new Dotenv(),
   ],
 };
