5fbbcd75de269fea20b114f6598920333c238577
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-07-25 15:47:36 +0300

    ADD weather update time and weather conditions
    - Add information about when weather forecast has been updated
    - Modify date and time according to local (Finnish) format

--- src/modules/currentTimeAndTemperature.js
+++ src/modules/currentTimeAndTemperature.js
@@ -22,6 +22,12 @@ function currentTimeAndTemperature(json) {
   const locationName = document.createElement("div");
   locationName.classList.add("locationName");
 
+  const currentWeatherConditions = document.createElement("div");
+  currentWeatherConditions.classList.add("currentWeatherConditions");
+
+  const currentWeatherConditionsText = document.createElement("p");
+  currentWeatherConditionsText.classList.add("currentWeatherConditionsText");
+
   const currentTempContainer = document.createElement("div");
   currentTempContainer.classList.add("currentTempContainer");
 
@@ -32,11 +38,24 @@ function currentTimeAndTemperature(json) {
   currentTempIcon.classList.add("weatherIcon");
 
   // ACCESS API AND FORMAT DATA
+  // Last Upadated
+  const timeForUpdate = new Date(json.current.last_updated);
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const weekDay = days[timeForUpdate.getDay()];
+  const day = timeForUpdate.getDate();
+  const month = timeForUpdate.getMonth() + 1;
+  const hours = timeForUpdate.getHours();
+  const minutePre = timeForUpdate.getMinutes();
+  const minutes = minutePre < 10 ? "0" + minutePre : minutePre;
+  const updated = `${weekDay} ${day}.${month} at ${hours}:${minutes}`;
 
   // Add data to elements
   currentTempIcon.src = json.current.condition.icon;
+  currentWeatherConditions.textContent = `Weather condition:`;
+  currentWeatherConditionsText.textContent = `${json.current.condition.text}`;
   locationName.textContent = `${json.location.name}, ${json.location.country}`;
   currentTemp.textContent = `${json.current.temp_c}°C`;
+  currentDate.textContent = `${updated}`;
 
   // Update page
   function updateLocation() {
@@ -46,8 +65,11 @@ function currentTimeAndTemperature(json) {
     }
     // Add new content
     dateTimeLocationContainer.appendChild(locationName);
+    dateTimeLocationContainer.appendChild(currentDate);
     dateTimeLocationContainer.appendChild(currentTemp);
     dateTimeLocationContainer.appendChild(currentTempIcon);
+    currentWeatherConditions.appendChild(currentWeatherConditionsText);
+    dateTimeLocationContainer.appendChild(currentWeatherConditions);
 
     mainContent.appendChild(dateTimeLocationContainer);
     container.appendChild(mainContent);
