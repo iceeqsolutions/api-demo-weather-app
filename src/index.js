d1b9b01e48db17924cda03a31c86c5bd70a65698
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-07-25 16:25:45 +0300

    ADD functionality to search bar
    - Add event listener to search bar to capture desired location
    - Add event listener for Enter key so that submit button can be omitted
    - Clear search field after submission for better user experience

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./styles.css";
 import getData from "./modules/getWeatherData";
 
 const mainContent = document.querySelector(".mainContent");
+const searchBar = document.querySelector("#searchLocation");
+let newLocation;
 
 // Initialize page
 function loadPage() {
@@ -12,3 +14,35 @@ function loadPage() {
   }
   
   loadPage();
+
+// Activate search bar
+searchBar.addEventListener("input", recordInput);
+searchBar.addEventListener("keypress", recordKey);
+
+// Save input from search bar
+function recordInput(evt) {
+  evt.preventDefault();
+  newLocation = evt.target.value;
+}
+
+// Run API search on "Enter"
+function recordKey(evt) {
+  if (evt.key === "Enter") {
+    evt.preventDefault();
+    updateLocation();
+  }
+}
+
+function updateLocation() {
+  const mainContent = document.querySelector(".mainContent");
+
+  const ics = document.querySelector(".ics-container");
+  while (mainContent.firstChild) {
+    mainContent.removeChild(mainContent.firstChild);
+  }
+
+  const location = newLocation ? newLocation : "helsinki";
+
+  getData(location);
+  searchBar.value = "";
+}
